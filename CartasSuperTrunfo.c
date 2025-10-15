5ce4c7ad5bb99f1213667932dd4fa980ed189286
LucasCGB <lucascamposgodoy@gmail.com>
2025-10-15 09:42:09 -0300

    Update CartasSuperTrunfo.c
    Foi acrescentado o desafio nível mestre, incrementando com o Super Poder e comparando as características de cada carta.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,6 +21,9 @@ int main() {
         int pontosturisticos0, pontosturisticos1; //A quantidade de pontos turisticos na cidade.
         float densiPopula0, densiPopula1; // Calcula a densidade populacional.
         float pibCapta0, pibCapta1; // Calcula PIB per capta.
+        float superPoder0, superPoder1; // Calcula Super Poder
+        int resultPop, resultArea, resultPib, resultTurist, resultDensi, resultCapta, resultSP;
+        float densiInversa0, densiInversa1;
  
         // leitura dos dados da primeira carta.
         printf("Digite os dados da primeira carta \n");
@@ -78,10 +81,36 @@ int main() {
         densiPopula1 = populacao1 / area1;
         pibCapta1 = (pib1 * 1000000000.0) / populacao1;
 
+        // ** Nível mestre **
+
+        densiInversa0 = 1 / densiPopula0;
+        densiInversa1 = 1 / densiPopula1;
+        
+        // Vamos incrementar com o Super Poder
+        superPoder0 = (float) populacao0 + area0 + pib0 + (float) pontosturisticos0 + pibCapta0 + densiInversa0;
+        superPoder1 = (float) populacao1 + area1 + pib1 + (float) pontosturisticos1 + pibCapta1 + densiInversa1;
+
         // Dados das Cartas 1 e 2
 
-        printf("Carta 1: \nEstado: %s \n Codigo: %s\n Nome da Cidade: %s\n Populacao: %d\n Area: %.2f km²\n PIB: %.2f bilhões de reais\n Numero de Pontos Turisticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capta: %.2f reais\n", estado0, codigocarta0, nomecidade0, populacao0, area0, pib0, pontosturisticos0, densiPopula0, pibCapta0);
-        printf("Carta 2: \nEstado: %s \n Codigo: %s\n Nome da Cidade: %s\n Populacao: %d\n Area: %.2f km²\n PIB: %.2f bilhões de reais\n Numero de Pontos Turisticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capta: %.2f reais\n", estado1, codigocarta1, nomecidade1, populacao1, area1, pib1, pontosturisticos1, densiPopula1, pibCapta1);
+        printf("Carta 1: \nEstado: %s \n Codigo: %s\n Nome da Cidade: %s\n Populacao: %d\n Area: %.2f km²\n PIB: %.2f bilhões de reais\n Numero de Pontos Turisticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capta: %.2f reais\n Super Poder: %.2f\n", estado0, codigocarta0, nomecidade0, populacao0, area0, pib0, pontosturisticos0, densiPopula0, pibCapta0, superPoder0);
+        printf("Carta 2: \nEstado: %s \n Codigo: %s\n Nome da Cidade: %s\n Populacao: %d\n Area: %.2f km²\n PIB: %.2f bilhões de reais\n Numero de Pontos Turisticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capta: %.2f reais\n Super Poder: %.2f\n", estado1, codigocarta1, nomecidade1, populacao1, area1, pib1, pontosturisticos1, densiPopula1, pibCapta1, superPoder1);
 
+        // comparar resultados das cartas, resultado "1" carta "A" ganha, resultado "0" carta "B" ganha.
+        resultPop = populacao0 > populacao1;
+        resultArea = area0 > area1;
+        resultPib = pib0 > pib1;
+        resultTurist = pontosturisticos0 > pontosturisticos1;
+        resultDensi = densiPopula0 > densiPopula1;
+        resultCapta = pibCapta0 > pibCapta1;
+        resultSP = superPoder0 > superPoder1;
+        
+        printf("O resultado da comparação entre a População da carta A e B é: %d\n", resultPop);
+        printf("O resultado da comparação entre a Área da carta A e B é: %d\n", resultArea);
+        printf("O resultado da comparação entre a PIB da carta A e B é: %d\n", resultPib);
+        printf("O resultado da comparação entre a Pontos Turísticos da carta A e B é: %d\n", resultTurist);
+        printf("O resultado da comparação entre a Densidade Populacional A e B é: %d\n", resultDensi);
+        printf("O resultado da comparação entre a PIB per Capta da carta A e B é: %d\n", resultCapta);
+        printf("O resultado da comparação entre a Super Poder da carta A e B é: %d\n", resultSP);
+       
 return 0;
 } 
